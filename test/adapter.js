71d4bce9d4f6464bd5347a1d3f1b3b3e3e0b70dc
lscspirit <lscspirit@hotmail.com>
2016-05-28 19:46:33 +0800

    Simplify test cases and add cases for props update

--- test/adapter.js
+++ test/adapter.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import { expect } from 'chai';
+import assign from 'object-assign';
 import LinkedStateAdapter from '../adapter';
 
 const faker = require('faker');
@@ -12,6 +13,15 @@ const faker = require('faker');
 //
 
 describe('LinkedStateAdapter', function() {
+  var mockComponentState = null;
+
+  //
+  // Setup
+  //
+  beforeEach(function() {
+    mockComponentState = new MockState();
+  });
+
   //
   // Text Input
   //
@@ -20,12 +30,48 @@ describe('LinkedStateAdapter', function() {
     // Value Link
     //
     context('using valueLink', function() {
-      it('should set the input value to the "value" state', function() {
-        assertSetValue(buildWrapper(TextInput, 'valueLink'), faker.lorem.sentence());
+      it('sets the input value to the "value" state', function() {
+        mockComponentState.setState({ value: faker.lorem.sentence() });
+
+        // link the state to the TextInput via valueLink
+        const wrapper = mount(<TextInput valueLink={ mockComponentState.linkState('value') }/>);
+        expect(wrapper.find('input').prop('value')).to.eq(mockComponentState.get('value'));
       });
 
-      it('should update the "value" state when input changes', function() {
-        assertValueUpdate(buildWrapper(TextInput, 'valueLink'), faker.lorem.sentence());
+      it('updates the "value" state when input changes', function() {
+        const txtVal = faker.lorem.sentence();
+
+        // link the state to the TextInput via valueLink
+        const wrapper = mount(<TextInput valueLink={ mockComponentState.linkState('value') }/>);
+        // change the text input
+        wrapper.find('input').simulate('change', { target: { value: txtVal } });
+        expect(mockComponentState.get('value')).to.eq(txtVal);
+      });
+
+      it('updates the input value when valueLink changes', function() {
+        mockComponentState.setState({
+          oldValue: faker.lorem.sentence(),
+          newValue: faker.lorem.sentence()
+        });
+
+        const wrapper = mount(<TextInput valueLink={ mockComponentState.linkState('oldValue') }/>);
+        // changes the valueLink prop
+        wrapper.setProps({ valueLink: mockComponentState.linkState('newValue') });
+        expect(wrapper.find('input').prop('value')).to.eq(mockComponentState.get('newValue'));
+      });
+
+      it('updates the change handler when valueLink changes', function() {
+        const oldTxt = faker.lorem.sentence();
+        const newTxt = faker.lorem.sentence();
+
+        const wrapper = mount(<TextInput valueLink={ mockComponentState.linkState('oldValue') }/>);
+        wrapper.find('input').simulate('change', { target: { value: oldTxt } });
+        // changes the valueLink prop
+        wrapper.setProps({ valueLink: mockComponentState.linkState('newValue') });
+        wrapper.find('input').simulate('change', { target: { value: newTxt } });
+
+        expect(mockComponentState.get('oldValue')).to.eq(oldTxt);
+        expect(mockComponentState.get('newValue')).to.eq(newTxt);
       });
     });
 
@@ -33,12 +79,52 @@ describe('LinkedStateAdapter', function() {
     // Value and onChange
     //
     context('using "value" and "onChange" props', function() {
-      it('should set the input value to the "value" state', function() {
-        assertSetValue(buildWrapper(TextInput, 'value'), faker.lorem.sentence());
+      var changeHandler = function(key) {
+        return function(e) {
+          var state = {};
+          state[key] = e.target.value;
+          mockComponentState.setState(state);
+        };
+      };
+
+      it('sets the input value to the "value" state', function() {
+        mockComponentState.setState({ value: faker.lorem.sentence() });
+
+        // set the 'value' and 'onChange' of the TextInput
+        const wrapper = mount(<TextInput value={ mockComponentState.get('value') } onChange={ changeHandler('value') }/>);
+        expect(wrapper.find('input').prop('value')).to.eq(mockComponentState.get('value'));
       });
 
-      it('should update the "value" state when input changes', function() {
-        assertValueUpdate(buildWrapper(TextInput, 'value'), faker.lorem.sentence());
+      it('updates the "value" state when input changes', function() {
+        const txtVal = faker.lorem.sentence();
+
+        // set the 'value' and 'onChange' of the TextInput
+        const wrapper = mount(<TextInput value={ mockComponentState.get('value') } onChange={ changeHandler('value') }/>);
+        // change the text input
+        wrapper.find('input').simulate('change', { target: { value: txtVal } });
+        expect(mockComponentState.get('value')).to.eq(txtVal);
+      });
+
+      it('updates the input value when "value" prop changes', function() {
+        const newTxt = faker.lorem.sentence();
+
+        const wrapper = mount(<TextInput value={ faker.lorem.sentence() }/>);
+        wrapper.setProps({ value: newTxt });
+        expect(wrapper.find('input').prop('value')).to.eq(newTxt);
+      });
+
+      it('updates the change handler when "onChange" prop changes', function() {
+        const oldTxt = faker.lorem.sentence();
+        const newTxt = faker.lorem.sentence();
+
+        const wrapper = mount(<TextInput onChange={ changeHandler('oldValue') }/>);
+        wrapper.find('input').simulate('change', { target: { value: oldTxt } });
+        // change the "onChange" prop
+        wrapper.setProps({ onChange: changeHandler('newValue') });
+        wrapper.find('input').simulate('change', { target: { value: newTxt } });
+
+        expect(mockComponentState.get('oldValue')).to.eq(oldTxt);
+        expect(mockComponentState.get('newValue')).to.eq(newTxt);
       });
     });
   });
@@ -51,12 +137,50 @@ describe('LinkedStateAdapter', function() {
     // Checked Link
     //
     context('using checkedLink', function() {
-      it('should set the checked property according to the "value" state', function() {
-        assertSetChecked(buildWrapper(CheckboxInput, 'checkedLink'), faker.random.boolean());
+      it('sets the checked property according to the "value" state', function() {
+        mockComponentState.setState({ checked: faker.random.boolean() });
+
+        // link the state to the CheckboxInput via checkedLink
+        const wrapper = mount(<CheckboxInput checkedLink={ mockComponentState.linkState('checked') }/>);
+        expect(wrapper.find('input').prop('checked')).to.eq(mockComponentState.get('checked'));
       });
 
-      it('should update the "value" state when toggled', function() {
-        assertCheckedToggle(buildWrapper(CheckboxInput, 'checkedLink'), faker.random.boolean());
+      it('updates the "value" state when toggled', function() {
+        const isChecked = faker.random.boolean();
+        mockComponentState.setState({ checked: isChecked });
+
+        // link the state to the CheckboxInput via checkedLink
+        const wrapper = mount(<CheckboxInput checkedLink={ mockComponentState.linkState('checked') }/>);
+        // flip the checked state
+        wrapper.find('input').simulate('change', { target: { checked: !!isChecked } });
+        expect(mockComponentState.get('checked')).to.eq(!!isChecked);
+      });
+
+      it('updates the input value when valueLink changes', function() {
+        const isOldChecked = faker.random.boolean();
+        mockComponentState.setState({
+          oldChecked: isOldChecked,
+          newChecked: !isOldChecked
+        });
+
+        const wrapper = mount(<CheckboxInput checkedLink={ mockComponentState.linkState('oldChecked') }/>);
+        // changes the checkedLink prop
+        wrapper.setProps({ checkedLink: mockComponentState.linkState('newChecked') });
+        expect(wrapper.find('input').prop('checked')).to.eq(mockComponentState.get('newChecked'));
+      });
+
+      it('updates the change handler when checkedLink changes', function() {
+        const oldChecked = faker.random.boolean();
+        const newChecked = !oldChecked;
+
+        const wrapper = mount(<CheckboxInput checkedLink={ mockComponentState.linkState('oldChecked') }/>);
+        wrapper.find('input').simulate('change', { target: { checked: oldChecked } });
+        // changes the valueLink prop
+        wrapper.setProps({ checkedLink: mockComponentState.linkState('newChecked') });
+        wrapper.find('input').simulate('change', { target: { checked: newChecked } });
+
+        expect(mockComponentState.get('oldChecked')).to.eq(oldChecked);
+        expect(mockComponentState.get('newChecked')).to.eq(newChecked);
       });
     });
 
@@ -64,96 +188,88 @@ describe('LinkedStateAdapter', function() {
     // Checked and onChange
     //
     context('using "checked" and "onChange" props', function() {
-      it('should set the checked property according to the "value" state', function() {
-        assertSetChecked(buildWrapper(CheckboxInput, 'checked'), faker.random.boolean());
+      var changeHandler = function(key) {
+        return function(e) {
+          var state = {};
+          state[key] = e.target.checked;
+          mockComponentState.setState(state);
+        };
+      };
+
+      it('sets the checked property according to the "value" state', function() {
+        mockComponentState.setState({ checked: faker.random.boolean() });
+
+        // set the 'checked' and 'onChange' prop of CheckboxInput
+        const wrapper = mount(<CheckboxInput checked={ mockComponentState.get('checked') } onChange={ changeHandler('checked') }/>);
+        expect(wrapper.find('input').prop('checked')).to.eq(mockComponentState.get('checked'));
       });
 
-      it('should update the "value" state when toggled', function() {
-        assertCheckedToggle(buildWrapper(CheckboxInput, 'checked'), faker.random.boolean());
+      it('updates the "value" state when toggled', function() {
+        const isChecked = faker.random.boolean();
+        mockComponentState.setState({ checked: isChecked });
+
+        // set the 'checked' and 'onChange' prop of CheckboxInput
+        const wrapper = mount(<CheckboxInput checked={ mockComponentState.get('checked') } onChange={ changeHandler('checked') }/>);
+        // flip the checked state
+        wrapper.find('input').simulate('change', { target: { checked: !!isChecked } });
+        expect(mockComponentState.get('checked')).to.eq(!!isChecked);
+      });
+
+      it('updates the input value when "checked" prop changes', function() {
+        const isOldChecked = faker.random.boolean();
+        const wrapper = mount(<CheckboxInput checked={ isOldChecked }/>);
+        // changes the checkedLink prop
+        wrapper.setProps({ checked: !isOldChecked });
+        expect(wrapper.find('input').prop('checked')).to.eq(!isOldChecked);
+      });
+
+      it('updates the change handler when checkedLink changes', function() {
+        const oldChecked = faker.random.boolean();
+        const newChecked = !oldChecked;
+
+        const wrapper = mount(<CheckboxInput onChange={ changeHandler('oldChecked') }/>);
+        wrapper.find('input').simulate('change', { target: { checked: oldChecked } });
+        // changes the valueLink prop
+        wrapper.setProps({ onChange: changeHandler('newChecked') });
+        wrapper.find('input').simulate('change', { target: { checked: newChecked } });
+
+        expect(mockComponentState.get('oldChecked')).to.eq(oldChecked);
+        expect(mockComponentState.get('newChecked')).to.eq(newChecked);
       });
     });
   });
 });
 
 //
-// Test methods for different types of input
+// Mock React Component State
 //
 
-function assertSetValue(WrapperElement, value) {
-  // mount the wrapper element
-  const wrapper = mount(<WrapperElement initialValue={ value }/>);
+function MockState(initial) {
+  var _state = initial || {};
+  var _stateSettters = {};
 
-  // check the value props on the input element
-  expect(wrapper.find('input').prop('value')).to.eq(value);
-}
+  this.get = function(key) {
+    return _state[key];
+  };
 
-function assertValueUpdate(WrapperElement, newValue) {
-  // mount the wrapper element
-  const wrapper = mount(<WrapperElement/>);
-  wrapper.find('input').simulate('change', { target: { value: newValue } });
+  this.setState = function(states) {
+    assign(_state, states);
+  };
 
-  // check the latest value state of the wrapper
-  expect(wrapper.state('value')).to.eq(newValue);
-}
+  this.linkState = function(key) {
+    return {
+      value: this.get(key),
+      requestChange: _getStateSetter(key)
+    };
+  };
 
-function assertSetChecked(WrapperElement, isChecked) {
-  // mount the wrapper element
-  const wrapper = mount(<WrapperElement initialValue={ isChecked }/>);
+  //
+  // Private Method
+  //
 
-  // check the value props of the input element
-  expect(wrapper.find('input').prop('checked')).to.eq(isChecked);
-}
-
-function assertCheckedToggle(WrapperElement, initialChecked) {
-  // mount the wrapper element
-  const wrapper = mount(<WrapperElement initialValue={ initialChecked }/>);
-  // toggle the input
-  wrapper.find('input').simulate('change', { target: { checked: !initialChecked } });
-
-  // check the latest value in the wrapper
-  expect(wrapper.state('value')).to.eq(!initialChecked);
-}
-
-//
-// Wrapper Builder
-//
-
-function buildWrapper(Input, type) {
-  return React.createClass({
-    mixins : [LinkedStateAdapter],
-
-    getInitialState : function() {
-      return { value : ('initialValue' in this.props) ? this.props.initialValue : '' };
-    },
-
-    render : function() {
-      if (type == 'valueLink') {
-        return <Input valueLink={ this._linkState() }/>;
-      } else if (type == 'checkedLink') {
-        return <Input checkedLink={ this._linkState() }/>;
-      } else if (type == 'checked') {
-        return <Input checked={ this.state.value } onChange={ this._onChange }/>;
-      }
-
-      return <Input value={ this.state.value } onChange={ this._onChange }/>;
-    },
-
-    _onChange : function(e) {
-      var val = type == 'checked' ? e.target.checked : e.target.value;
-      this.setState({ value : val });
-    },
-
-    _linkState : function() {
-      var self = this;
-
-      return {
-        value: this.state.value,
-        requestChange: function(newValue) {
-          self.setState({ value : newValue });
-        }
-      };
-    }
-  });
+  function _getStateSetter(key) {
+    return _stateSettters[key] || (_stateSettters[key] = function(val) { _state[key] = val; });
+  }
 }
 
 //
